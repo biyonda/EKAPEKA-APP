2df85f7c095740807249def1b7237a76d889a360
Biyonda Kurniawan / LV <biyondakurniawan@gmail.com>
2022-05-28 19:10:26 +0700

    tampilan upload dokumen

---
+++ app/src/main/java/com/example/ekpkdisnaker/activity/PasFotoActivity.java
@@ -0,0 +1,31 @@
+package com.example.ekpkdisnaker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.ekpkdisnaker.R;
+
+public class PasFotoActivity extends AppCompatActivity {
+
+    ImageView btn_back;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pas_foto);
+
+        btn_back = findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/ekpkdisnaker/activity/SettingActivity.java
+++ app/src/main/java/com/example/ekpkdisnaker/activity/SettingActivity.java
@@ -2,15 +2,18 @@ package com.example.ekpkdisnaker.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.ekpkdisnaker.R;
 
 public class SettingActivity extends AppCompatActivity {
 
     ImageView btn_back;
+    RelativeLayout btn_pas,btn_ktp, btn_ijazah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,40 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         btn_back = findViewById(R.id.btn_back);
+        btn_pas = findViewById(R.id.btn_pas);
+        btn_ktp = findViewById(R.id.btn_ktp);
+        btn_ijazah = findViewById(R.id.btn_ijazah);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btn_pas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, PasFotoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn_ktp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, KTPActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn_ijazah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, IjazahActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
