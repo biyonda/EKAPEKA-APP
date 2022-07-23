7ac99ee1f7549e6467b6411e7d2deb543b83a371
Biyonda Kurniawan / LV <biyondakurniawan@gmail.com>
2022-07-23 20:34:35 +0700

    update

---
+++ app/src/main/java/com/example/ekpkdisnaker/activity/DetailPengumumanActivity.java
@@ -0,0 +1,16 @@
+package com.example.ekpkdisnaker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ekpkdisnaker.R;
+
+public class DetailPengumumanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_pengumuman);
+    }
+}
