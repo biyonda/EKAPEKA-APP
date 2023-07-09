516fed506f2d7dda59b029ebbe32cf8b36545ed1
Biyonda Kurniawan / LV <biyondakurniawan@gmail.com>
2023-07-09 14:56:06 +0700

    sementara

---
+++ app/src/main/java/com/diskominfo/ekpkdisnaker/activity/TambahAK1Activity.java
@@ -0,0 +1,16 @@
+package com.diskominfo.ekpkdisnaker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.diskominfo.ekpkdisnaker.R;
+
+public class TambahAK1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_ak1);
+    }
+}
