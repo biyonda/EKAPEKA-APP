ed14030ba0caa664a143c5e0ce0446f6d349dc8c
Biyonda Kurniawan / LV <biyondakurniawan@gmail.com>
2023-07-22 23:45:15 +0700

    layout peminatan

---
+++ app/src/main/java/com/diskominfo/ekpkdisnaker/activity/PeminatanActivity.java
@@ -0,0 +1,16 @@
+package com.diskominfo.ekpkdisnaker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.diskominfo.ekpkdisnaker.R;
+
+public class PeminatanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peminatan);
+    }
+}
